fc02699569b2513684d69ee86f61673e99c95a2b
adrianromanski <adrian.romanski@codeweaver.pl>
2024-06-23 09:47:14 +0200

    tests: add calculator add, subtract cases

---
+++ app/app/test.py
@@ -0,0 +1,14 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+        self.assertEquals(res, 11)
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(15, 10)
+
+        self.assertEquals(res, 5)
